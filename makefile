af52b2e1b61ed82a60c2c6b11e77abe7a3b3fe7a
Prodge <prodge@prodge.net>
2016-01-24 17:32:46 +0800

    Early progress, Started working with sqlite

--- makefile
+++ makefile
@@ -3,11 +3,15 @@ FLAGS = -std=c99 -Wall -Werror -pedantic -c
 EXE = rmr
 BINDIR = /usr/local/bin
 MANDIR = /usr/local/share/man/man1
+LIBRARYS = -lpthread -ldl
 
 all: $(EXE)
 
 rmr: rmr.o; \
-    $(COMPILER) -o $(EXE) rmr.o
+    $(COMPILER) -o $(EXE) rmr.o sqlite3.o $(LIBRARYS)
+
+sqlite3.o: sqlite3.c; \
+    $(COMPILER) $(FLAGS) sqlite3.c
 
 rmr.o: rmr.c; \
     $(COMPILER) $(FLAGS) rmr.c
--- rmr.c
+++ rmr.c
@@ -10,6 +10,11 @@
  *  - 'db' for storing file information
  *  - 'log' for storing log information
  *  - All files being recycled in the 'tmp/' folder
+ *
+ * Dependencies:
+ *  - Sqlite3
+ *      - libsqlite3-dev
+ *      - sqlite3
  */
 
 #include <stdio.h>
@@ -20,12 +25,26 @@
 #include <getopt.h>
 #include <ctype.h>
 #include <unistd.h>
+#include <sqlite3.h>
+
+/*Upper bound for path length*/
+int PATH_MAX_LENGTH = 511;
 
 /*Setting mode to verbose by default*/
 bool verbose = false;
 
 /*Folder containing rmr recycle bin and database*/
-char folder[] = ".rmr";
+char *folder = ".rmr";
+
+/*Options list*/
+char *options = "d:a:sr:nhcv";
+
+/*The directory from which rmr is being run*/
+char *currentDirectory;
+
+/*Database Pointer*/
+sqlite3 *db;
+
 
 /*
  * Writes the input string to the log
@@ -62,20 +81,24 @@ char* concat(char *string1, char *string2){
  */
 bool canDelete(char *filename){
     bool state = true;
+
     // Validating file Exists
-    if(access(filename, F_OK) == -1)
+    if(access(filename, F_OK) == -1){
         toLog(concat("File does not exist: ", filename));
         state = false;
+    }
 
     // Validating Read Permission
-    if(access(filename, R_OK) == -1)
+    if(access(filename, R_OK) == -1){
         toLog(concat("User does not have read permission for file: ", filename));
         state = false;
+    }
 
     // Validating Write Permission
-    if(access(filename, W_OK) == -1)
+    if(access(filename, W_OK) == -1){
         toLog(concat("User does not have write permission for file: ", filename));
         state = false;
+    }
 
     return state;
 }
@@ -103,37 +126,86 @@ bool delete(char *filename){
 }
 
 /*
- * Returns true if the system has been initialised
+ * Returns true if the system has been initialized
  *      .rmr folder exists
  *      DB exists and is in valid state
  *      recycle bin folder exists
  */
-bool isInitialised(){
+bool isInitialized(){
     //implement this
-    return true;
+    return false;
 }
 
 /*
- * Initialises the system for rmr
+ * Initializes the system for rmr
  *      Creates the rmr folder
  *      Creates the db
  *      Creates the recycle bin
  *      Creates the log file
  */
-bool initialise(){
-    return true;
+void initialize(){
+
+    // if no rmr folder exists
+    //  create rmr folder
+
+    // if no db exists
+
+    // Creating database
+    toLog("System requires initialization");
+    if(sqlite3_open("test.db", &db)){
+        fprintf(stderr, "Cant open database%s\n", sqlite3_errmsg(db));
+        exit(EXIT_FAILURE);
+    }else{
+        toLog("Opened Database Successfully");
+    }
+
+    // Creating tables
+    char *zErrMsg = 0;
+    char *sql = "CREATE TABLE FILES("  \
+            "ID INT PRIMARY KEY     NOT NULL," \
+            "NAME           TEXT    NOT NULL," \
+            "AGE            INT     NOT NULL," \
+            "ADDRESS        CHAR(50)," \
+            "SALARY         REAL );";
+
+    if(sqlite3_exec(db, sql, NULL, 0, &zErrMsg) == SQLITE_OK){
+        toLog("Successfully Created Files Table");
+    }else{
+        fprintf(stderr, "Failed to create Files table");
+        exit(EXIT_FAILURE);
+    }
+
+
+
+    toLog("Successfully Initialized rmr");
+}
+
+/*
+ * Moves the given file to the rmr directory permenantly
+ */
+void archive(char *filename){
+    toLog(concat("Attempting to archive: ", filename));
+
+
 }
 
 
-
 int main(int argc, char **argv){
-    //Initialise the system for rmr if it is not already
-    if(! isInitialised()){
-        if(! initialise()){
-            // The system failed to initialise
-            // Print to stderr
-        }
+    verbose = true;
+    // Gotting the current working directory
+    char buff[PATH_MAX_LENGTH + 1];
+    currentDirectory = getcwd(buff, PATH_MAX_LENGTH + 1);
+    if(currentDirectory != NULL){
+        toLog(concat("Current working directory set to: ", currentDirectory));
+    }else{
+        fprintf(stderr, "Could not get current working directory\n");
+        exit(EXIT_FAILURE);
     }
+
+    // Initialize the system for rmr if it is not already
+    if(! isInitialized())
+        initialize();
+
 
     int flag;
     if(argc == 1){
@@ -144,13 +216,13 @@ int main(int argc, char **argv){
         printf("Server not yet implemented\n");
         //sleep(60);
     }else{
-        while((flag = getopt(argc, argv, "d:k:sr:nhcv")) != -1){
+        while((flag = getopt(argc, argv, options)) != -1){
             switch(flag){
                 case 'd':   //Delete file for d days
                     printf("Delete is Not yet implemented\n");
                     break;
                 case 'a':   //archive to rmrtmp and never delete
-                    printf("Archive not yet implemented\n");
+                    archive(optarg);
                     break;
                 case 's':   //Display status for all files rmrtmp
                     printf("Not yet implemented\n");
@@ -173,4 +245,6 @@ int main(int argc, char **argv){
             }
         }
     }
+    sqlite3_close(db);
+    exit(EXIT_SUCCESS);
 }
